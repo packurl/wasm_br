693d87892ef0d655962e6e88ed25958d6037c2b9
Jerome David <programingjd@gmail.com>
2022-11-19 17:18:04 +0100

    Added decoder-only version.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use brotli::{BrotliCompress, BrotliDecompress};
-// use brotli::enc::backward_references::BrotliEncoderMode::BROTLI_MODE_TEXT;
+// use brotli::{/*BrotliCompress,*/ BrotliDecompress};
 use wasm_bindgen::prelude::*;
 use brotli::enc::BrotliEncoderParams;
 
@@ -14,7 +14,7 @@ pub fn unbr(data: &[u8]) -> Vec<u8> {
 pub fn br(data: &[u8]) -> Vec<u8> {
     let mut out = Vec::with_capacity(data.len() + 64);
     let mut params= BrotliEncoderParams::default();
-    // params.mode = BROTLI_MODE_TEXT;
+    // params.mode = brotli::enc::backward_references::BrotliEncoderMode::BROTLI_MODE_TEXT;
     params.large_window = true;
     params.size_hint = data.len();
     BrotliCompress(&mut &*data, &mut out, &params).unwrap();
