b80bc4aa729e797e7d651bfabc57661d59a569de
programingjd <programingjd@gmail.com>
2022-12-31 03:17:39 +0100

    Disable large-window option.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use brotli::{BrotliCompress, BrotliDecompress};
-// use brotli::{/*BrotliCompress,*/ BrotliDecompress};
-use wasm_bindgen::prelude::*;
 use brotli::enc::BrotliEncoderParams;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn unbr(data: &[u8]) -> Vec<u8> {
@@ -14,9 +13,14 @@ pub fn unbr(data: &[u8]) -> Vec<u8> {
 pub fn br(data: &[u8]) -> Vec<u8> {
     let mut out = Vec::with_capacity(data.len() + 64);
     let mut params= BrotliEncoderParams::default();
+    // Setting the mode to text doesn't seem to be necessary as it's detected correctly
+    // and since it produces larger files on non text files, it's better not to set it.
     // params.mode = brotli::enc::backward_references::BrotliEncoderMode::BROTLI_MODE_TEXT;
-    params.large_window = true;
+    // large_window option seem to render compressed file incompatible with curl and browsers
+    // content decoding.
+    // params.large_window = true;
     params.size_hint = data.len();
     BrotliCompress(&mut &*data, &mut out, &params).unwrap();
     out
 }
+
