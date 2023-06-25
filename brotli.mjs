3e4c3a2fd669acddfa7bc91e414c5890abf56b3c
programingjd <programingjd@gmail.com>
2023-06-25 14:16:26 +0200

    Reset pointer in wrappers.

--- brotli.mjs
+++ brotli.mjs
@@ -6,16 +6,18 @@ const src=name=>`(async()=>{
   const malloc=wasm.__wbindgen_malloc;const free=wasm.__wbindgen_free;
   const pointer=wasm.__wbindgen_add_to_stack_pointer;
   const fn=it=>{
-    const r=pointer(-16);
     const n1=it.length;
     const p1=malloc(n1,1);
-    new Uint8Array(wasm.memory.buffer).set(it,p1);
-    wasm.${name}(r,p1,n1);
-    const arr=new Int32Array(wasm.memory.buffer);
-    const p2=arr[r/4];const n2=arr[r/4+1];
-    const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
-    free(p2,n2);
-    return res;
+    const r=pointer(-16);
+    try{
+      new Uint8Array(wasm.memory.buffer).set(it,p1);
+      wasm.${name}(r,p1,n1);
+      const arr=new Int32Array(wasm.memory.buffer);
+      const p2=arr[r/4];const n2=arr[r/4+1];
+      const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
+      free(p2,n2);
+      return res;
+    }finally{pointer(16)}
   };
   onmessage=async msg=>postMessage(fn(msg.data));
   postMessage('ready');
