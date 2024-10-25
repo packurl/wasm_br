118009922f96dd16d4e459520e5bb8cd9332755c
programingjd <programingjd@gmail.com>
2024-10-25 12:50:40 +0200

    Update dependencies and scripts

--- brotli_dec.mjs
+++ brotli_dec.mjs
@@ -1,8 +1,20 @@
 const url=new URL('./brotli_dec.wasm',import.meta.url);
-const {instance:{exports:wasm}}=await WebAssembly.instantiateStreaming(await fetch(url,{cache:'force-cache'}),{wbg:{}});
+let wasm;
+const wbg={
+  __wbindgen_init_externref_table: function(){
+    const table=wasm.__wbindgen_export_0;
+    const offset=table.grow(4);
+    table.set(0);
+    table.set(offset);
+    table.set(offset+1,null);
+    table.set(offset+2,true);
+    table.set(offset+3,false);
+  }
+};
+const {instance}=await WebAssembly.instantiateStreaming(await fetch(url,{cache:'force-cache'}),{wbg});
+wasm=instance.exports;
 const malloc=wasm.__wbindgen_malloc;
 const free=wasm.__wbindgen_free;
-const pointer=wasm.__wbindgen_add_to_stack_pointer;
 /**
  * Decompresses an array of bytes compressed with Brotli.
  * @param {Uint8Array} bytes
@@ -11,16 +23,11 @@ const pointer=wasm.__wbindgen_add_to_stack_pointer;
 const unbr=bytes=>{
   const n1=bytes.length;
   const p1=malloc(n1,1);
-  const r=pointer(-16);
-  try{
-    new Uint8Array(wasm.memory.buffer).set(bytes,p1);
-    wasm.unbr(r,p1,n1);
-    const arr=new Int32Array(wasm.memory.buffer);
-    const p2=arr[r/4];const n2=arr[r/4+1];
-    const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
-    free(p2,n2);
-    return res;
-  }finally{pointer(16)}
+  new Uint8Array(wasm.memory.buffer).set(bytes,p1);
+  const [p2,n2]=wasm.unbr(p1,n1);
+  const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
+  free(p2,n2);
+  return res;
 };
 export {unbr};
 export default unbr;
--- brotli_enc.mjs
+++ brotli_enc.mjs
@@ -1,8 +1,20 @@
 const url=new URL('./brotli_enc.wasm',import.meta.url);
-const {instance:{exports:wasm}}=await WebAssembly.instantiateStreaming(await fetch(url,{cache:'force-cache'}),{wbg:{}});
+let wasm;
+const wbg={
+  __wbindgen_init_externref_table: function(){
+    const table=wasm.__wbindgen_export_0;
+    const offset=table.grow(4);
+    table.set(0);
+    table.set(offset);
+    table.set(offset+1,null);
+    table.set(offset+2,true);
+    table.set(offset+3,false);
+  }
+};
+const {instance}=await WebAssembly.instantiateStreaming(await fetch(url,{cache:'force-cache'}),{wbg});
+wasm=instance.exports;
 const malloc=wasm.__wbindgen_malloc;
 const free=wasm.__wbindgen_free;
-const pointer=wasm.__wbindgen_add_to_stack_pointer;
 /**
  * Compresses an array of bytes with Brotli.
  * @param {Uint8Array} bytes
@@ -12,16 +24,11 @@ const pointer=wasm.__wbindgen_add_to_stack_pointer;
 const br=(bytes,level=11)=>{
   const n1=bytes.length;
   const p1=malloc(n1,1);
-  const r=pointer(-16);
-  try{
-    new Uint8Array(wasm.memory.buffer).set(bytes,p1);
-    wasm.br(r,p1,n1,level);
-    const arr=new Int32Array(wasm.memory.buffer);
-    const p2=arr[r/4];const n2=arr[r/4+1];
-    const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
-    free(p2,n2);
-    return res;
-  }finally{pointer(16)}
+  new Uint8Array(wasm.memory.buffer).set(bytes,p1);
+  const [p2,n2]=wasm.br(p1,n1,level);
+  const res=new Uint8Array(wasm.memory.buffer).subarray(p2,p2+n2).slice();
+  free(p2,n2);
+  return res;
 };
 export {br};
 export default br;
